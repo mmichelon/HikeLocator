dac55ce9d324d04552743277285618715097d2f0
mmichelon <maxmichelon15@gmail.com>
2019-03-11 18:52:17 -0700

    Drawer Test

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,12 +1,7 @@
-// This sample shows creation of a [Card] widget that shows album information
-// and two actions.
 import '../models/trail_model.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import '../screens/map_screen.dart';
-
-import 'package:flutter/material.dart';
-
 class ListScreen extends StatelessWidget {
   final List<TrailModel> trails;
   final userLat;
@@ -30,97 +25,47 @@ class TrailList extends StatelessWidget{
   final double userLon;
 
   TrailList(this.trails, this.userLat, this.userLon);
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Code Sample for material.Card',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TrailCard(),
-    );
-  }
-}
 
-class TrailCard extends StatelessWidget {
-  TrailCard({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.Builder(
-//      return Center(
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Title")),
+    body: Center(child: Text('My Page!')),
+    drawer: Drawer(
+    // Add a ListView to the drawer. This ensures the user can scroll
+    // through the options in the Drawer if there isn't enough vertical
+    // space to fit everything.
+    child: ListView.builder(
       itemCount: trails.length,
-      child: Card(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            itemBuilder: (context, int index) {
-              Object myText = json.encode(trails[index].trails);
-              List<dynamic> myText2 = json.decode(myText);
-            if(myText2.length > index) {
-                ListTile(
-                  leading: Icon(Icons.album),
-                  title: Text(myText2[index]['name']),
-                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-                ),
-                ButtonTheme.bar(
-                  // make buttons use the appropriate styles for cards
-                  child: ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('BUY TICKETS'),
-                        onPressed: () {/* ... */},
-                      ),
-                      FlatButton(
-                        child: const Text('LISTEN'),
-                        onPressed: () {/* ... */},
-                      ),
-                    ],
-                  ),
-                ),
-              }
-            }
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Card extends StatelessWidget {
-  Card ({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.album),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-            ),
-            ButtonTheme.bar(
-              // make buttons use the appropriate styles for cards
-              child: ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: const Text('BUY TICKETS'),
-                    onPressed: () {/* ... */},
-                  ),
-                  FlatButton(
-                    child: const Text('LISTEN'),
-                    onPressed: () {/* ... */},
-                  ),
-                ],
+      itemBuilder: (context, int index) {
+        Object myText = json.encode(trails[index].trails);
+        List<dynamic> myText2 = json.decode(myText);
+        if(myText2.length > index) {
+          return ListTile(
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: 20.0, vertical: 10.0),
+            leading: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
               ),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(10.0),
+              child: Text(myText2[index]['name']),
             ),
-          ],
-        ),
-      ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => new MapScreen(
+                      myText2[index]['latitude'], myText2[index]['longitude'],
+                      userLat, userLon),
+                ),
+              );
+            },
+          );
+        }
+      },
+    )
+    )
     );
   }
 }
