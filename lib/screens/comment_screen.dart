09e01028e58b1090efb82578a54257d5c2c4faec
Lindsay Wommack <l.wommack.7@gmail.com>
2019-04-15 16:05:06 -0700

    Hike Comments fully functional

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -1,39 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import "dart:async";
-import "login_screen.dart"; //for current user
 
-//THINGS_TO_DO:
-//I need to take in the hike's information (id # & name)
-//Access the current user's username & id
 
 class CommentScreen extends StatefulWidget {
   final List<dynamic> newTrails;
   final int curIndex;
   const CommentScreen({Key key, this.newTrails, this.curIndex}): super(key: key);
-//  const CommentScreen({Key key, this.curIndex}): super(key: key);
 
   @override
   _CommentScreenState createState() => _CommentScreenState();
-//  CommentScreen({this.newTrails, this.curIndex});
-
-//  @override
-//  _CommentScreenState createState() {
-//    return _CommentScreenState(newTrails: newTrails, curIndex: curIndex);
-//  }
-//  _CommentScreenState createState() => _CommentScreenState(
-//      newTrails: this.newTrails,
-//      curIndex: this.curIndex
-//  );
 }
 
 class _CommentScreenState extends State<CommentScreen> {
-//  final List<dynamic> newTrails;
-//  final int curIndex;
-
   final TextEditingController _commentController = new TextEditingController();
-
-//  _CommentScreenState({this.newTrails, this.curIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -96,43 +76,31 @@ class _CommentScreenState extends State<CommentScreen> {
     return comments;
   }
 
-  addComment(String comment) {
+  addComment(String comment) async{
     _commentController.clear();
     Firestore.instance
         .collection("hikes")
         .document(widget.newTrails[0][widget.curIndex]['id'].toString())
         .collection("comments")
         .add({
-      "username": mCurrentUser,
       "comment": comment,
       "timestamp": DateTime.now().toString(),
-//      "avatarUrl": mCurrentUser.photoUrl,
-//      "userId": mCurrentUser.id
     });
   }
 }
 
 class Comment extends StatelessWidget {
-  final String username;
-  final String userId;
-//  final String avatarUrl;
   final String comment;
   final String timestamp;
 
   Comment(
-      {this.username,
-        this.userId,
-//        this.avatarUrl,
-        this.comment,
+      { this.comment,
         this.timestamp});
 
   factory Comment.fromDocument(DocumentSnapshot document) {
     return Comment(
-      username: document['username'],
-      userId: document['userId'],
       comment: document["comment"],
       timestamp: document["timestamp"],
-//      avatarUrl: document["avatarUrl"],
     );
   }
 
@@ -142,10 +110,8 @@ class Comment extends StatelessWidget {
       children: <Widget>[
         ListTile(
           title: Text(comment),
-            leading: Text(username, style: TextStyle(color: Color.fromRGBO(58, 66, 86, 1.0))),
-//          leading: CircleAvatar(
-//            backgroundImage: NetworkImage(avatarUrl),
-//          ),
+          leading: Text("Anonymous User: ", style: TextStyle(color: Color.fromRGBO(58, 66, 86, 1.0))),
+//          trailing: Text(timestamp, style: TextStyle(color: Color.fromRGBO(58, 66, 86, 1.0))),
         ),
         Divider(),
       ],
